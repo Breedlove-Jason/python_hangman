5b65915e6ca3736720c87918d8dd3db6d67e7083
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-16 08:48:38 -0700

    update display with guess and game loop complete

--- hangman.py
+++ hangman.py
@@ -2,14 +2,32 @@ import random
 
 word_list = ["ardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 print(chosen_word)
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-print(guess)
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+
+print(display)
+lives = 5
+while "_" in display or lives > 0:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You have {lives} lives left")
+    if lives == 0:
+        print("You lose")
+        break
+
+    if "_" not in display:
+        print("You win")
+        break
+
