5c9087faf1245508f3ea4633404ff8150dd0fb75
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-16 09:12:13 -0700

    added lives and ascii art for gallows

--- hangman.py
+++ hangman.py
@@ -2,15 +2,74 @@ import random
 
 word_list = ["ardvark", "baboon", "camel"]
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+print(stages[6])
 display = []
 for letter in chosen_word:
     display.append("_")
 
 print(display)
-lives = 5
+lives = 6
 while "_" in display or lives > 0:
     guess = input("Guess a letter: ").lower()
 
@@ -22,11 +81,11 @@ while "_" in display or lives > 0:
     print(display)
     if guess not in chosen_word:
         lives -= 1
+        print(stages[lives])
         print(f"You have {lives} lives left")
-    if lives == 0:
-        print("You lose")
-        break
-
+        if lives == 0:
+            print("You lose")
+            break
     if "_" not in display:
         print("You win")
         break
